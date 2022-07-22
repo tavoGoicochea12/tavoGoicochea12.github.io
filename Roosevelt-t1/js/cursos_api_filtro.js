4b51b053610499b0a5c7abcc77de59547c32bae7
tavoGoicochea12 <109829894+tavoGoicochea12@users.noreply.github.com>
2022-07-22 17:20:42 -0500

    Add files via upload

---
+++ Roosevelt-t1/js/cursos_api_filtro.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  var $grid = $(".grid").isotope({
+    itemSelector: ".grid-item",
+  });
+
+  // filter items on button click
+  $("ul.filter-menu").on("click", "li", function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+    return false;
+  });
+
+  $("ul.filter-menu li").click(function () {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+  });
+});
